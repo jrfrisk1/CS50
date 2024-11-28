b5594882e341db59fb81cf596a624bca0354af51
jrfrisk1 <108193186+jrfrisk1@users.noreply.github.com>
2024-11-27 19:09:44 -0600

    Added Event Menu Template

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app.secret_key = 'hello'  #TODO HIDE
 # Ensure sessions is temp
 app.config['SESSION_PERMANENT'] = False
 
+
 # Store admin info
 ADMIN_USER = 'admin' #TODO HIDE
 ADMIN_PASS = '123' #TODO HIDE
+
 
 # Admin login
 @app.route('/admin', methods=['GET', 'POST'])
@@ -27,38 +29,43 @@ def admin():
             return render_template('admin.html', error='Invalid Login')
     return render_template('admin.html')
 
+
 # Admin Dash
-@app.route('/admin-dash')
+@app.route('/admin-dash', methods=['GET', 'POST'])
 def admindash():
     if 'username' in session:
         return render_template('admindash.html')
     return render_template('admin.html', error='Timed Out')
+
 
 # Submit Event
 @app.route('/submit-event')
 def submitevent():
     return render_template('submitevent.html')
 
+
 # HomePage
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # Events
 @app.route('/events')
 def events():
     return render_template('events.html')
 
+
 # Whats Kava
 @app.route('/whats-kava')
 def whatskava():
     return render_template('whatskava.html')
 
+
 # Contact Us
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
 
 
 # Run the application
